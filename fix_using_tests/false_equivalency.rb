599e17ea15fcf1992ba013f4c6c6970b6343958c
Adam Shilling <admshng@gmail.com>
2020-08-27 18:03:29 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -13,10 +13,10 @@ def prompt_user
 end
 
 def selection(num)
-if num = 1
-     "YUM YUM MUNCH MUNCH MUNCH"
-  else 
+  if num = 2 
      "HAM HAM HAM IN MY TUMMY"
+  elsif num = 1
+     "YUM YUM MUNCH MUNCH MUNCH"
    end
 end
 
